f21517acde5e71775075478bbbd415a29e9d0cb9
Samuel Mensah <samuelnmensah@live.com>
2017-04-26 12:53:31 -0700

    added project

---
+++ src/Developerbackpack.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Developerbackpack.Site.MvcApplication" Language="C#" %>
---
+++ src/Developerbackpack.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Developerbackpack.Site.Startup))]
+namespace Developerbackpack.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
